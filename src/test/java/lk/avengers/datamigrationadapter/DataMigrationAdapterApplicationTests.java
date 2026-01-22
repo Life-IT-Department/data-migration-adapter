93ad82e0c694f38a7101b8163f4b965e731e433e
Prabash Dahanayake <prabash.dahanayake@softlogiclife.lk>
2026-01-22 09:37:25 +0530

    initial commit - prabash

---
+++ src/test/java/lk/avengers/datamigrationadapter/DataMigrationAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.avengers.datamigrationadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
