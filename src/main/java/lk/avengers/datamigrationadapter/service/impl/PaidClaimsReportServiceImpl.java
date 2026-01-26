def345ddc2f45395adc5693db102ddd4acc0a740
Prabash Dahanayake <prabash.dahanayake@softlogiclife.lk>
2026-01-26 13:09:02 +0530

    pos signature report upload

--- src/main/java/lk/avengers/datamigrationadapter/service/impl/PaidClaimsReportServiceImpl.java
+++ src/main/java/lk/avengers/datamigrationadapter/service/impl/PaidClaimsReportServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PaidClaimsReportServiceImpl implements PaidClaimsReportService {
 
-    @Value("paidClaimReport.file")
+    @Value("${paidClaimReport.file}")
     String paidClaimReportFilePath;
 
     private final PaidClaimReportRepository paidClaimReportRepository;
