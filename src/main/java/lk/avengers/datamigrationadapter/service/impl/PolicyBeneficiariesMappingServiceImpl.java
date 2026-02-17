ffe8deff6acc41f7abf043214870d9639887c77b
pulasthi <pulasthinarada@hotmail.com>
2026-02-17 15:08:45 +0530

    Update PolicyBeneficiariesMappingServiceImpl.java

--- src/main/java/lk/avengers/datamigrationadapter/service/impl/PolicyBeneficiariesMappingServiceImpl.java
+++ src/main/java/lk/avengers/datamigrationadapter/service/impl/PolicyBeneficiariesMappingServiceImpl.java
@@ -47,7 +47,7 @@ public class PolicyBeneficiariesMappingServiceImpl implements PolicyBeneficiarie
             "SCL/935254", "SCL/935395", "SCL/935486", "SCL/935510", "ULF/527879", "UPR/215038", "ULP/321661", "SCL/1038926", "UPR/399550", "SCL/900753", "ULV/451021", "ULV/571323", "UPR/126334", "SCL/988394");
 
 
-    //private final List<String> policyList = List.of("ULT/348672");
+    //private final List<String> policyList = List.of("ALH/756080");
 
     @Transactional(transactionManager = "softlogicPlatformTransactionManager")
     @Override
